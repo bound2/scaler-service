01657be3c4932c6d9bd6ff2c052a0890577a4118
bound2 <9380102+bound2@users.noreply.github.com>
2021-05-20 18:38:31 +0300

    add required to url

--- scaler/Api/Request/ImageScalerRequest.cs
+++ scaler/Api/Request/ImageScalerRequest.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace scaler.Api.Request
 {
     public class ImageScalerRequest
     {
+        [Required]
         public string Url { get; set; }
     }
 }
